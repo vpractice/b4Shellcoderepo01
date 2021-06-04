f892cb080bb4ce97fe9f5b33542d2fc0d4bec87f
Kashif Bhatti <kashif@vpractice.pk>
2021-06-04 22:31:52 +0500

    files updated

--- scriptfile8.sh
+++ scriptfile8.sh
@@ -2,6 +2,7 @@
 
 echo "new file added in local repo"
 echo "new lines added via remote repo"
+<<<<<<< HEAD
 
 echo "new line"`
 echo "new line"`
@@ -10,3 +11,5 @@ echo "new line"`
 echo "new line"`
 echo "new file added in local repo"
 echo "new lines added via remote repo"
+=======
+>>>>>>> parent of d2eb82f... file updated
